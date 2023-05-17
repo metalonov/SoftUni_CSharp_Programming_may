dada10f4c15005db4e2c83062207ff6585753618
metalonov <118399788+metalonov@users.noreply.github.com>
2023-05-17 20:40:09 +0300

    Update1

---
+++ 10. Rage Expenses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SqlTypes;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double kbdPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsetCount = 0;
+            int mouseCount = 0;
+            int kbdCount = 0;
+            int displayCount = 0;
+            bool sameGame = false;
+            bool sameGameTwo = false;
+
+            //for loop based on lostGamesCount
+
+            for (int i = 1; i <= lostGamesCount; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    headsetCount++;
+                    sameGame = true;
+                }
+                if (i % 3 == 0) 
+                {
+                    mouseCount++;
+                    sameGameTwo = true;
+                }
+                if (sameGame == true && sameGameTwo == true)
+                {
+                    kbdCount++;
+                    if (kbdCount > 0 && kbdCount % 2 == 0)
+                    {
+                        displayCount++;
+                    }
+                }
+                sameGame = false;
+                sameGameTwo = false;
+            }
+            double gearCost = (headsetPrice * (double)headsetCount) + (mousePrice * (double)mouseCount) + ((double)kbdCount * kbdPrice) + ((double)displayCount * displayPrice);
+            Console.WriteLine($"Rage expenses: {gearCost:f2} lv.");
+        }
+    }
+}
---
+++ 9. Padawan Equipment/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _9._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = float.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double sabersPrice = float.Parse(Console.ReadLine());
+            double robesPrice = float.Parse(Console.ReadLine());
+            double beltsPrice = float.Parse(Console.ReadLine());
+
+            double sabersCountTotal = students + (students * 0.1);
+            double sabersPriceTotal = Math.Ceiling(sabersCountTotal) * sabersPrice; 
+            double robesPriceTotal = robesPrice * students;
+            double freeBelts = students / 6;
+            double beltsPriceTotal = beltsPrice * students - beltsPrice * Math.Floor(freeBelts);
+            double subTotal = sabersPriceTotal + robesPriceTotal + beltsPriceTotal;
+            if (subTotal <= budget) 
+            {
+            Console.WriteLine($"The money is enough - it would cost {subTotal:f2}lv.");
+            }
+            else 
+            {
+            
+            Console.WriteLine($"John will need {subTotal - budget:f2}lv more.");
+            }
+        }
+    }
+}
