964a6af33fbb429fb7dbe0da364e61a1563170b6
metalonov <118399788+metalonov@users.noreply.github.com>
2023-05-20 06:37:55 +0300

    commit

---
+++ Exercise Data Types and Variables/10. Pokemon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+namespace _10._Pokemon
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());  //poke power the Pokemon has, N – an integer.
+            int distance = int.Parse(Console.ReadLine()); //distance between the poke targets, M – an integer
+            int exhaustionFactor = int.Parse(Console.ReadLine());  //the exhaustionFactor Y – an integer
+            int targetCount = 0;
+            int halfPower = pokePower/2;
+
+            while (pokePower >= distance) //until N becomes less than M
+            {
+                            //start subtracting M
+                            //from N 
+                pokePower -= distance;
+                if (pokePower == halfPower) //IF N becomes equal to EXACTLY 50 % of its original value
+                {
+                    pokePower /= exhaustionFactor; //you must divide N by Y
+                }
+                targetCount++;
+            }
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetCount);
+        }
+    }
+}
